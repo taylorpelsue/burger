c720e4348240e63e7f629a1239eb9e3f6d399d45
Taylor Pelsue <taylorpelsue@gmail.com>
2018-05-02 18:38:27 -0500

    initial commit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var burger = require("../models/burger.js");
+var router = express.Router();
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.create(["burger_name", "devour"], [req.body.burger_name, req.body.devour], function (result) {
+    
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update(
+        {
+            devour: req.body.devour
+        },
+        condition,
+        function (result) {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+
+        }
+    );
+});
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baby You Can Chive My Car Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Havarti Like it's 1999 Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('If Looks Could Kale Burger', false);
