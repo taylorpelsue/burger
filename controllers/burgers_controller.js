ae308dc782f10337634e0e1bb3f7d4c00b6e8fa5
Taylor Pelsue <taylorpelsue@gmail.com>
2018-05-02 23:10:06 -0500

    main content edited

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,7 +13,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.create(["burger_name", "devour"], [req.body.burger_name, req.body.devour], function (result) {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
     
         res.json({ id: result.insertId });
     });
@@ -26,7 +26,7 @@ router.put("/api/burgers/:id", function (req, res) {
 
     burger.update(
         {
-            devour: req.body.devour
+            devoured: req.body.devoured
         },
         condition,
         function (result) {
